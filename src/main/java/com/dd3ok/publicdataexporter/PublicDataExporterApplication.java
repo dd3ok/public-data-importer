d0d932a3fe9bd32302f440f75094c784c39a914a
dd3ok <hwick@nate.com>
2025-06-18 21:42:22 +0900

    init project

---
+++ src/main/java/com/dd3ok/publicdataexporter/PublicDataExporterApplication.java
@@ -0,0 +1,13 @@
+package com.dd3ok.publicdataexporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicDataExporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PublicDataExporterApplication.class, args);
+    }
+
+}
